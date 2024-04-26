2298713b2a490c72232d309880ed094a785b8fae
andymoggo <andymoggo@gmail.com>
2024-04-26 18:07:15 -0500

    First route - App Routing

--- lib/app.ts
+++ lib/app.ts
@@ -2,21 +2,24 @@
 
 import express = require('express');
 import bodyParser = require('body-parser');
+import { Routes } from './routes/myRoutes';
 
 class App {
 
     public app: express.Application;
+    public routerMyApp : Routes = new Routes;
 
     constructor() {
         this.app = express();
         this.config();
+        this.routerMyApp.routes(this.app);
     }
 
     private config() : void {
         //Enable support for POST data of type json.
-        this.app.use(bodyParser);
+        this.app.use(express.json());
         //Enable support for POST data of type x-www-urlencoded.
-        this.app.use(bodyParser.urlencoded({ extended : false}));
+        this.app.use(express.urlencoded({ extended : true}));
     }
 }
 
---
+++ lib/routes/myRoutes.ts
@@ -0,0 +1,15 @@
+// lib/routes/myRoutes.ts
+
+import { Application, Request, Response} from 'express';
+
+export class Routes {
+    public routes(app: Application): void {
+        app.route('/')
+        .get((req: Request, res: Response) => {
+            console.log("Get Request received");
+            res.status(200).send({
+                message: 'GET REQUEST RECEIVED'
+            });
+        });
+    };
+}
